996df0295927abf0d28f9531a2de32046a339de3
semen123123 <pacific123coast@gmail.com>
2024-04-09 00:08:18 +0300

    1day

--- app/src/main/java/com/example/myapplication111/onboard.java
+++ app/src/main/java/com/example/myapplication111/onboard.java
@@ -2,7 +2,11 @@ package com.example.myapplication111;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+
 
 public class onboard extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class onboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboard);
+    }
+
+    public void OnClick(View v) {
+        Intent intent = new Intent(onboard.this, onboard2.class);
+        startActivity(intent);
+    }
+
+    public void OnClick1(View v) {
+        Intent intent = new Intent(onboard.this, onboard3.class);
+        startActivity(intent);
     }
 }
